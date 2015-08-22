a123bd97539497fd95791f80442d3ca2c4496534
Rahim Dharssi <rahim.dharssi@gmail.com>
2015-08-21 18:41:50 -0700

    added reduce function

--- rahim_.js
+++ rahim_.js
@@ -1,14 +1,25 @@
 //Transforms an array based on the "iteratee" function provided
 
-function each(Arr, iteratee) {
-	for (var i = 0; i < Arr.length; i++) {
-		Arr[i] = iteratee(numArr[i]);
+function each(arr, iteratee) {
+	for (var i = 0; i < arr.length; i++) {
+		arr[i] = iteratee(numArr[i]);
 	}
 }
 
 //Similar to each, but creates a new array
-function map(Arr, iteratee) {
-	var newArr = Arr.slice(0);
+function map(arr, iteratee) {
+	var newArr = arr.slice(0);
 	each(newArr, iteratee);
 	return newArr;
+}
+
+//Reduce function
+function reduce(arr, reduceFunc, memo) {
+	if (arr.length > 0 && memo !== undefined) {
+		return reduce(arr.slice(1), reduceFunc, reduceFunc(memo, arr[0]))
+	} else if (arr.length > 0) {
+		return reduce(arr.slice(1), reduceFunc, arr[0])
+	} else {
+		return memo;
+	}
 }
